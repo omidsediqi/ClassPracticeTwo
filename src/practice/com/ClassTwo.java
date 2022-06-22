158acf0b8fe9f5c23f708accf021d53deddf4b9d
omid.sediqi <omid.sediqi55@hotmail.com>
2022-06-21 20:36:36 -0700

    this is repeat code

---
+++ src/practice/com/ClassTwo.java
@@ -0,0 +1,14 @@
+package practice.com;
+
+public class ClassTwo {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 50;
+		int sum = a+b;
+		
+		System.out.println(sum);
+
+	}
+
+}
